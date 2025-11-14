77a8fe851273f8fcfbd8d44c61425174d7eeb76e
Eric Pedley <ericpedley@gmail.com>
2025-11-14 08:59:46 -0800

    move updates into loop

--- rl_eval.py
+++ rl_eval.py
@@ -6,13 +6,13 @@ import numpy as np
 def eval_reward_with_controller(env: TinyPhysicsEnv, controller: BaseController):
     done = False
     obs, _ = env.reset()
-    target_lataccel = obs[(3+3)*env.policy_history_len-1]
-    current_lataccel = obs[(2+3)*env.policy_history_len-1]
-    state = obs[(3*env.policy_history_len-3):3*env.policy_history_len]
-    future_plan = obs[6*env.policy_history_len:].reshape((4,-1))
     
     rewards = []
     while not done:
+        target_lataccel = obs[(3+3)*env.policy_history_len-1]
+        current_lataccel = obs[(2+3)*env.policy_history_len-1]
+        state = obs[(3*env.policy_history_len-3):3*env.policy_history_len]
+        future_plan = obs[6*env.policy_history_len:].reshape((4,-1))
         action = [controller.update(target_lataccel, current_lataccel, state, future_plan)]
         obs, rew, term, truncated, _= env.step(action)
         done = term or truncated

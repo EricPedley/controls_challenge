2284b7f97fcfd94a4a08013ee870cd89ad489d9c
Eric Pedley <ericpedley@gmail.com>
2025-11-14 19:37:54 -0800

    start vectorizing env

--- env.py
+++ env.py
@@ -1,26 +1,25 @@
-from tinyphysics import TinyPhysicsModel, TinyPhysicsSimulator, CONTEXT_LENGTH, MAX_ACC_DELTA, CONTROL_START_IDX, STEER_RANGE, State, FuturePlan, COST_END_IDX, LAT_ACCEL_COST_MULTIPLIER , DEL_T
+from tinyphysics import TinyPhysicsModel, TinyPhysicsSimulator, CONTEXT_LENGTH, MAX_ACC_DELTA, CONTROL_START_IDX, STEER_RANGE, State, FuturePlan, COST_END_IDX, LAT_ACCEL_COST_MULTIPLIER , DEL_T, FUTURE_PLAN_STEPS
 import numpy as np
-from gymnasium import Env
+from gymnasium.vector import VectorEnv
 from gymnasium.spaces import Box
 from controllers import BaseController
 from pathlib import Path
-from random import choice
+import torch
 
-class TinyPhysicsEnv(Env, TinyPhysicsSimulator):
-    def __init__(self, model_path='models/tinyphysics.onnx'):
-        self.model_path = model_path
+class TinyPhysicsEnv(VectorEnv):
+    def __init__(self, num_envs=1):
+        self.sim_model = TinyPhysicsModel('models/tinyphysics.onnx')
+        self.num_envs = num_envs
         self.previous_pred = 0
         self.policy_history_len = 4
         self.lookahead_len = 4
-        data_path = str(choice(list(Path('data').glob('*.csv'))))
-        TinyPhysicsSimulator.__init__(self, TinyPhysicsModel(self.model_path, False), data_path, BaseController())
-        Env.__init__(self)
         state_low = [-1,0,-0.1]
         state_high = [1,60,0.1]
         actions_low = [-2]
         actions_high = [2]
         preds_low = [-2]
         preds_high = [2]
+        self.all_data_paths = list(Path('data').glob('*.csv')) 
         self.observation_space = Box(
             np.concatenate([
                 np.tile(state_low, self.policy_history_len),
@@ -38,12 +37,31 @@ class TinyPhysicsEnv(Env, TinyPhysicsSimulator):
             ]),
         )
         self.action_space = Box(-2, 2, (1,))
+        self.data_dfs = [None]*self.num_envs
+        self.step_indices = torch.zeros(self.num_envs)
+        self.reset()
     
-    def step(self, action):
+    def step(self, actions: torch.Tensor):
+        assert actions.shape[0] == self.num_envs
+        assert actions.shape[1] == 1
+
         state, target, futureplan = self.get_state_target_futureplan(self.step_idx)
+        for env_idx in range(self.num_envs):
+            entire_state = self.data_dfs[env_idx].iloc[self.step_indices[env_idx]]
+            state = state['roll_lataccel'], state['v_ego'], state['a_ego']
+            target = state['target_lataccel']
+            future_plan = FuturePlan(
+                lataccel=self.data['target_lataccel'].values[self.step_indices[env_idx] + 1:self.step_indices[env_idx] + FUTURE_PLAN_STEPS].tolist(),
+                roll_lataccel=self.data['roll_lataccel'].values[self.step_indices[env_idx] + 1:self.step_indices[env_idx] + FUTURE_PLAN_STEPS].tolist(),
+                v_ego=self.data['v_ego'].values[self.step_indices[env_idx] + 1:self.step_indices[env_idx] + FUTURE_PLAN_STEPS].tolist(),
+                a_ego=self.data['a_ego'].values[self.step_indices[env_idx] + 1:self.step_indices[env_idx] + FUTURE_PLAN_STEPS].tolist()
+            )
+        
+
+
+
         self.state_history.append(state)
         self.target_lataccel_history.append(target)
-        self.futureplan = futureplan
 
         if self.step_idx < CONTROL_START_IDX:
             action = self.data['steer_command'].values[self.step_idx]
@@ -96,8 +114,14 @@ class TinyPhysicsEnv(Env, TinyPhysicsSimulator):
         info = dict()
         return obs, reward, terminated, truncated, info
     
+    def _reset_index(self, env_index: int):
+        assert env_index < self.num_envs
+        data_path = np.random.choice(self.all_data_paths)
+        self.data_dfs[env_index] = TinyPhysicsSimulator.get_data(None, path)
+    
     def reset(self, seed=None):
-        data_path = choice(list(Path('data').glob('*.csv')))
-        self.data=self.get_data(str(data_path))
-        TinyPhysicsSimulator.reset(self)
-        return self.step(0)[0], dict()
+        self.env_indices = np.zeros(self.num_envs, dtype=np.uint32)
+
+
+
+
